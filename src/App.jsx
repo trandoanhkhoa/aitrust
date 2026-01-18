e48c3d7579aae7c33aac855878e2aeab6faa96db
trandoanhkhoa <trandoanhkhoa192837@gmail.com>
2026-01-18 21:31:04 +0700

    fix small errors 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,6 @@ import DefaultLayout from './components/DefaultLayout';
 import PrivateRoute from './routes/Privateroutes';
 
 function App() {
-  useEffect(() => {
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-    if (!isSafari) return;
-
-    window.onerror = function (msg, url, line, col) {
-      document.body.innerHTML = `
-        <pre style="white-space:pre-wrap;padding:16px;font-size:14px;color:red">
-JS ERROR:
-${msg}
-
-Line: ${line}:${col}
-URL: ${url}
-        </pre>
-      `;
-    };
-
-    window.onunhandledrejection = function (e) {
-      document.body.innerHTML = `
-        <pre style="white-space:pre-wrap;padding:16px;font-size:14px;color:red">
-PROMISE ERROR:
-${String(e.reason)}
-        </pre>
-      `;
-    };
-
-    return () => {
-      window.onerror = null;
-      window.onunhandledrejection = null;
-    };
-  }, []);
-
   return (
     <Router>
       <Routes>
