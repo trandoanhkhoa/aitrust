c68b95b9dfefec255b1c8ef5ec0ada5f5e005336
trandoanhkhoa <trandoanhkhoa192837@gmail.com>
2026-01-25 15:28:12 +0700

    Fix link api

--- src/api/axiosClient.jsx
+++ src/api/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://localhost:44395/api',
+  baseURL: 'https://aitrustapi-production.up.railway.app/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
